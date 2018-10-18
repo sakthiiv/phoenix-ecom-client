48cbd376538068dcf36b22558a9f2c2987c766df
Arpit Kumar <arpit.kumar@INarpitkumar.local>
2018-10-18 17:48:49 +0530

    create product and category updated

--- src/components/CreateCategory.js
+++ src/components/CreateCategory.js
@@ -2,27 +2,44 @@ import React, { Component } from 'react';
 
 class Admin extends React.Component {
     state = {
-      subcats: [{name:""}],
+      subcatsDom: [],
       cname: "",
-      description: ""
+      description: "",
+      subcats:[]
     }
   handleChange = (e) => {
-      if (["name"].includes(e.target.className) ) {
-        let subcats = [...this.state.subcats]
-        subcats[e.target.dataset.id][e.target.className] = e.target.value.toUpperCase()
-        this.setState({ subcats }, () => console.log(this.state.subcats))
+      if (e.target.className.indexOf('name') !== -1) {
+        // let subcats = [...this.state.subcats, {name:e.target.value}]
+        // subcats[e.target.dataset.id][e.target.className] = e.target.value
+        // this.setState({ subcats }, () => console.log(this.state.subcats))
+        let newSub = {
+            name:e.target.value
+        }
+        this.setState(prevState => ({
+            subcats: [...prevState.subcats, newSub]
+        }));
+        // let newelement = {
+        //     id: Math.random()
+        //       .toString(36)
+        //       .substr(2, 9),
+        //     author: this.state.author,
+        //     comment: this.state.comment
+        //   };
+          
       } else {
-        this.setState({ [e.target.name]: e.target.value.toUpperCase() })
+        this.setState({ [e.target.name]: e.target.value })
       }
     }
     addSubCat = (e) => {
         this.setState((prevState) => ({
-            subcats: [...prevState.subcats, {name:""}],
+            subcatsDom: [...prevState.subcatsDom, {name:""}],
         }));
     }
-  handleSubmit = (e) => { e.preventDefault() }
+  handleSubmit = (e) => { e.preventDefault() 
+    console.log(this.state);
+  }
   render() {
-      let {cname, description, subcats} = this.state
+      let {cname, description, subcatsDom} = this.state
       return (
         <div className="col-md-6">
             <h1>Create category</h1>
@@ -30,17 +47,17 @@ class Admin extends React.Component {
 
                 <div className="form-group">
                     <label htmlFor="cname">Enter name of the categpry</label>
-                    <input id='cname' type="text" placeholder='Enter name of the category' className="form-control" defaultValue={cname}></input>
+                    <input id='cname' name="cname" type="text" placeholder='Enter name of the category' className="form-control" defaultValue={cname}></input>
                 </div>
 
             <div className="form-group">
                     <label htmlFor="description">Example description of the category</label>
-                    <textarea className="form-control" id="description" rows="3" defaultValue={description}></textarea>
+                    <textarea className="form-control" name="description" id="description" rows="3" defaultValue={description}></textarea>
             </div>
 
             <button className="add-sub-cat-btn btn-success" onClick={this.addSubCat}>Add new subcategory</button>
             {
-                subcats.map((val, idx)=> {
+                subcatsDom.map((val, idx)=> {
                 let subCatId = `subcat-${idx}`
                 return (
                     <div key={idx}>
@@ -51,7 +68,7 @@ class Admin extends React.Component {
                                 name={subCatId}
                                 data-id={idx}
                                 id={subCatId}
-                                defaultValue={subcats[idx].name} 
+                                defaultValue={subcatsDom[idx].name} 
                                 className="name form-control"
                             />
                         </div>
--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -2,28 +2,92 @@ import React, { Component } from 'react';
 
 class Admin extends React.Component {
     state = {
-      subcats: [{name:""}],
       pname: "",
       pdescription: "",
-      price:""
+      price:"",
+      image: null,
+      imagePreviewUrl: null,
+      categoriesecom:null,
+      categories:['Electronics', 'Clothes'],
+      subcategories:[]
     }
-  handleChange = (e) => {
-      if (["name"].includes(e.target.className) ) {
-        let subcats = [...this.state.subcats]
-        subcats[e.target.dataset.id][e.target.className] = e.target.value.toUpperCase()
-        this.setState({ subcats }, () => console.log(this.state.subcats))
-      } else {
-        this.setState({ [e.target.name]: e.target.value.toUpperCase() })
-      }
+
+    componentDidMount() {
+        //initial categories
+        // let initialCategories = [];
+        // fetch('https://swapi.co/api/planets/')
+        //     .then(response => {
+        //         return response.json();
+        //     }).then(data => {
+        //         initialCategories = data.results.map((category) => {
+        //         return category
+        //     });
+        //     console.log(initialCategories);
+        //     this.setState({
+        //         categories: initialCategories
+        //     });
+        // });
+    }
+
+    handleChange = (e) => {
+        if (["file"].includes(e.target.className) ) {
+            let reader = new FileReader();
+            let file = e.target.files[0];
+
+            reader.onloadend = () => {
+                this.setState({
+                    image: file,
+                    imagePreviewUrl: reader.result
+                });
+            }
+            reader.readAsDataURL(file)
+        } else {
+            this.setState({ [e.target.name]: e.target.value })
+        }
     }
     addSubCat = (e) => {
         this.setState((prevState) => ({
             subcats: [...prevState.subcats, {name:""}],
         }));
     }
-  handleSubmit = (e) => { e.preventDefault() }
+  handleSubmit = (e) => { e.preventDefault() 
+    // console.log('PHOTO:', newItem.image);
+    const h = {}; //headers
+    let data = new FormData();
+    data.append('name', this.state.pname);
+    data.append('description', this.state.pdescription);
+    data.append('price', this.state.price);
+    data.append('image', this.state.image);
+    data.append('categoryId', this.state.categoriesecom);
+    data.append('subCategoryId', this.state.subcategoriesecom);
+
+    
+    h.Accept = 'application/json'; //if you expect JSON response
+    fetch('/api/item', {
+      method: 'POST',
+      headers: h,
+      body: data
+    }).then(response => {
+    }).catch(err => {
+    });
+  }
+
+
   render() {
-      let {pname, pdescription, price} = this.state
+      let {pname, pdescription, price, imagePreviewUrl, categories, subcategories} = this.state;
+      let $imagePreview = null;
+      if (imagePreviewUrl) {
+          $imagePreview = (<img src={imagePreviewUrl} className={'img-preview'} />);
+      } else {
+          $imagePreview = (<div className="previewText">Please select an image.</div>);
+      }
+
+      let categoriesItems = categories.map((category) =>
+            <option key={category}>{category}</option>
+      );
+      let subCategoriesItems = subcategories.map((subcategory) =>
+            <option key={subcategory}>{subcategory}</option>
+      );
       return (
         <div className="col-md-6">
             <h1>Create product</h1>
@@ -31,19 +95,40 @@ class Admin extends React.Component {
 
                 <div className="form-group">
                     <label htmlFor="pname">Enter name of the categpry</label>
-                    <input id='pname' type="text" placeholder='Enter name of the product' className="form-control" defaultValue={pname}></input>
+                    <input id='pname' type="text" name="pname" placeholder='Enter name of the product' className="form-control" defaultValue={pname}></input>
                 </div>
 
                 <div className="form-group">
                         <label htmlFor="pdescription">Example description of the category</label>
-                        <textarea className="form-control" id="pdescription" rows="3" defaultValue={pdescription}></textarea>
+                        <textarea className="form-control" id="pdescription" name="pdescription" rows="3" defaultValue={pdescription}></textarea>
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="price">Enter name of the categpry</label>
-                    <input id='price' type="text" placeholder='Enter price of the product' className="form-control" defaultValue={price}></input>
+                    <input id='price' name="price" type="text" placeholder='Enter price of the product' className="form-control" defaultValue={price}></input>
                 </div>
 
+                <div className="form-group">
+                    <label htmlFor="categoriesecom">Select category</label>
+                    <select className="form-control" id="categoriesecom" name="categoriesecom">
+                        {categoriesItems}
+                    </select>
+                </div>
+                {subcategories.length !== 0 && 
+                <div className="form-group">
+                    <label htmlFor="subcategoriesecom">Select subcategory</label>
+                    <select className="form-control" id="subcategoriesecom" name="subcategoriesecom">
+                        {subCategoriesItems}
+                    </select>
+                </div> }
+                
+
+                <div className="form-group">
+                    <input type="file"  name="file" className="file"/>
+                    <div className="img-preview">
+                        {$imagePreview}
+                    </div>
+                </div>
                 <input type="submit" value="Submit" /> 
             </form>
         </div>
